6ad9832a05e0443e85208ffae08aa7e1ed10a804
Russell Johnson <joh14233@umn.edu>
2021-06-23 15:46:45 -0700

    Adding Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro";
+import { HomePage } from "./app/containers/HomePage"
+
+const AppContainer = styled.div`${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+`}`;
 
 function App() {
-  return (
-    <div className="App">
-      YErr
-    </div>
-  );
+  return <AppContainer>
+      <HomePage />
+  </AppContainer>
 }
 
 export default App;
